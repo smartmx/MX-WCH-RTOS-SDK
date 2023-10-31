ed9edd5757a441526545e34e08af4970a8903ca1
smartmx <30313912+smartmx@users.noreply.github.com>
2023-10-31 15:17:46 +0800

    add ch592 liteos and rt-thread.

---
+++ CH592_1/LiteOS_m/Src/APP/main.c
@@ -0,0 +1,262 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.1
+ * Date               : 2022/05/10
+ * Description        : kernel_liteos_m例程，使用硬件压栈，禁用中断嵌套，中断函数不再使用修饰
+ *                      __attribute__((interrupt("WCH-Interrupt-fast")))，
+ *                      中断函数直接按照普通函数定义，只使用HIGHCODE修饰即可。
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * SPDX-License-Identifier: Apache-2.0
+ *******************************************************************************/
+
+/******************************************************************************/
+/* 头文件包含 */
+#include "CH59x_common.h"
+#include "config.h"
+#include "peripheral.h"
+#include "HAL.h"
+#include "stdarg.h"
+#include "los_tick.h"
+#include "los_task.h"
+#include "los_config.h"
+#include "los_interrupt.h"
+#include "los_debug.h"
+#include "los_compiler.h"
+#include "los_sem.h"
+
+static UINT32 g_semId;
+
+__attribute__((aligned(4))) uint32_t MEM_BUF[BLE_MEMHEAP_SIZE / 4];
+#if (defined (BLE_MAC)) && (BLE_MAC == TRUE)
+const uint8_t MacAddr[6] = {0x84, 0xC2, 0xE4, 0x03, 0x32, 0x22};
+#endif
+
+/*********************************************************************
+ * @fn      Main_Circulation
+ *
+ * @brief   Main loop
+ *
+ * @return  none
+ */
+__attribute__((section(".highcode")))
+__attribute__((noinline))
+void Main_Circulation(void)
+{
+    while(1)
+    {
+        TMOS_SystemProcess();
+    }
+}
+
+/*********************************************************************
+ * @fn      tmos_task
+ *
+ * @brief   tmos program.
+ *
+ * @param  *pvParameters - Parameters point of task2
+ *
+ * @return  none
+ */
+void tmos_task(void *pvParameters)
+{
+    UINT32 int_value;
+    int_value = LOS_IntLock();
+    CH59x_BLEInit();
+    HAL_Init();
+    GAPRole_PeripheralInit();
+    Peripheral_Init();
+
+#ifdef CH59xBLE_ROM
+    /* 固定库需要手动修改中断向量表 */
+    do
+    {
+        extern uint32_t _BB_IRQHandler_base[1];
+        _BB_IRQHandler_base[0] = (uint32_t)BB_IRQLibHandler;
+    } while (0);
+#endif
+
+    PRINT("%08x,%08x\n", PFIC->VTFADDR[2], PFIC->VTFADDR[3]);
+    SetVTFIRQ((uint32_t)0, BLEB_IRQn, 2, DISABLE);
+    SetVTFIRQ((uint32_t)0, BLEL_IRQn, 3, DISABLE);
+    PRINT("%08x,%08x\n", PFIC->VTFADDR[2], PFIC->VTFADDR[3]);
+
+    PFIC_SetPriority(BLEL_IRQn, 0xe0);   /* 必须保证不可抢断其他任务 */
+
+    LOS_IntRestore(int_value);
+
+    PRINT_INFO("tmos ble start\n");
+    Main_Circulation();
+}
+
+/*********************************************************************
+ * @fn      taskSampleEntry2
+ *
+ * @brief   taskSampleEntry2 program.
+ *
+ * @return  none
+ */
+VOID taskSampleEntry2(VOID)
+{
+    while(1) {
+      LOS_TaskDelay(10000);
+      PRINT("taskSampleEntry2 running,task2 SP:%08x\n",__get_SP());
+    }
+}
+
+/*********************************************************************
+ * @fn      taskSampleEntry1
+ *
+ * @brief   taskSampleEntry1 program.
+ *
+ * @return  none
+ */
+VOID taskSampleEntry1(VOID)
+{
+    while(1) {
+      LOS_TaskDelay(2000);
+      PRINT("taskSampleEntry1 running,task1 SP:%08x\n",__get_SP());
+    }
+}
+
+/*********************************************************************
+ * @fn      taskIntSem
+ *
+ * @brief   taskIntSem program.
+ *
+ * @return  none
+ */
+VOID taskIntSem(VOID)
+{
+    if(LOS_BinarySemCreate(0, &g_semId) == LOS_OK)
+    {
+        GPIOA_ModeCfg(GPIO_Pin_12, GPIO_ModeIN_PU);
+        GPIOA_ITModeCfg(GPIO_Pin_12, GPIO_ITMode_FallEdge);
+        PFIC_EnableIRQ(GPIO_A_IRQn);
+        while (1)
+        {
+            if(LOS_SemPend(g_semId, LOS_WAIT_FOREVER) == LOS_OK)
+            {
+                PRINT("sem get ok\n");
+            }
+            else
+            {
+                PRINT("sem get failed\n");
+            }
+        }
+    }
+    else
+    {
+        PRINT("sem create failed\n");
+    }
+}
+
+/*********************************************************************
+ * @fn      taskSample
+ *
+ * @brief   taskSample program.
+ *
+ * @return  none
+ */
+UINT32 taskSample(VOID)
+{
+    UINT32  uwRet;
+    UINT32 taskID1,taskID2,taskID3,taskIDIntSem;
+    TSK_INIT_PARAM_S stTask={0};
+
+    stTask.pfnTaskEntry = (TSK_ENTRY_FUNC)taskIntSem;
+    stTask.uwStackSize  = LOSCFG_BASE_CORE_TSK_DEFAULT_STACK_SIZE;
+    stTask.pcName       = "int sem test";
+    stTask.usTaskPrio   = 6;/* high priority */
+    uwRet = LOS_TaskCreate(&taskIDIntSem, &stTask);
+    if (uwRet != LOS_OK) {
+        PRINT("create int sem test failed\n");
+    }
+
+    stTask.pfnTaskEntry = (TSK_ENTRY_FUNC)taskSampleEntry1;
+    stTask.uwStackSize  = LOSCFG_BASE_CORE_TSK_DEFAULT_STACK_SIZE;
+    stTask.pcName       = "taskSampleEntry1";
+    stTask.usTaskPrio   = 6;/* high priority */
+    uwRet = LOS_TaskCreate(&taskID1, &stTask);
+    if (uwRet != LOS_OK) {
+        PRINT("create task1 failed\n");
+    }
+
+    stTask.pfnTaskEntry = (TSK_ENTRY_FUNC)taskSampleEntry2;
+    stTask.uwStackSize  = LOSCFG_BASE_CORE_TSK_DEFAULT_STACK_SIZE;
+    stTask.pcName       = "taskSampleEntry2";
+    stTask.usTaskPrio   = 7;/* low priority */
+    uwRet = LOS_TaskCreate(&taskID2, &stTask);
+    if (uwRet != LOS_OK) {
+        PRINT("create task2 failed\n");
+    }
+
+    stTask.pfnTaskEntry = (TSK_ENTRY_FUNC)tmos_task;
+    stTask.uwStackSize  = 0x500;
+    stTask.pcName       = "tmos_task";
+    stTask.usTaskPrio   = OS_TASK_PRIORITY_LOWEST - 2;  /* only 1 high than idle task priority */
+    uwRet = LOS_TaskCreate(&taskID3, &stTask);
+    if (uwRet != LOS_OK) {
+        PRINT("create tmos task failed\n");
+    }
+
+    return LOS_OK;
+}
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+LITE_OS_SEC_TEXT_INIT int main(void)
+{
+    unsigned int ret;
+
+#if (defined(DCDC_ENABLE)) && (DCDC_ENABLE == TRUE)
+    PWR_DCDCCfg(ENABLE);
+#endif
+    SetSysClock(CLK_SOURCE_PLL_60MHz);
+#if (defined(HAL_SLEEP)) && (HAL_SLEEP == TRUE)
+    GPIOA_ModeCfg(GPIO_Pin_All, GPIO_ModeIN_PU);
+    GPIOB_ModeCfg(GPIO_Pin_All, GPIO_ModeIN_PU);
+#endif
+#ifdef DEBUG
+    GPIOA_SetBits(bTXD1);
+    GPIOA_ModeCfg(bTXD1, GPIO_ModeOut_PP_5mA);
+    UART1_DefInit();
+#endif
+    PRINT("start.\n");
+
+    ret = LOS_KernelInit();
+    taskSample();
+    if (ret == LOS_OK)
+    {
+        LOS_Start();
+    }
+
+    PRINT("shouldn't run at here!!!\r\n");
+    while (1) {
+        __asm volatile("nop");
+    }
+
+    return 0;
+
+}
+
+__HIGH_CODE
+void GPIOA_IRQHandler(void)
+{
+    /* 本函数可以作为在本工程LiteOS中的中断函数写法示例 */
+    uint16_t flag;
+
+    flag = GPIOA_ReadITFlagPort();
+    if((flag & GPIO_Pin_12) != 0)
+    {
+        LOS_SemPost(g_semId);
+    }
+    GPIOA_ClearITFlagBit(flag); //清除中断标志
+}
+
+/******************************** endfile @ main ******************************/
---
+++ CH592_1/RT-Thread-Nano/Src/APP/main.c
@@ -0,0 +1,217 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : Main.c
+ * Author             : WCH
+ * Version            : V1.0
+ * Date               : 2020/08/06
+ * Description        : rt-thread nano移植例程，使用硬件压栈，中断嵌套可选，中断函数不再使用修饰
+ *                      __attribute__((interrupt("WCH-Interrupt-fast")))，
+ *                      中断函数直接按照普通函数定义，只使用HIGHCODE修饰即可。
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+#include "CH59x_common.h"
+#include <rtthread.h>
+#include "config.h"
+#include "HAL.h"
+#include "peripheral.h"
+#include "rthw.h"
+
+ALIGN(RT_ALIGN_SIZE)
+static char task1_stack[512];
+static struct rt_thread task1_thread;
+
+/*********************************************************************
+ * @fn      task1_entry
+ *
+ * @brief   task1任务函数
+ *
+ * @return  none
+ */
+void task1_entry(void *parameter)
+{
+    while(1)
+    {
+        rt_kprintf("task1\r\n");
+        rt_thread_delay(1000);
+    }
+}
+
+ALIGN(RT_ALIGN_SIZE)
+static char task2_stack[512];
+static struct rt_thread task2_thread;
+
+/*********************************************************************
+ * @fn      task2_entry
+ *
+ * @brief   task2任务函数
+ *
+ * @return  none
+ */
+void task2_entry(void *parameter)
+{
+    while(1)
+    {
+        rt_kprintf("task2\r\n");
+        rt_thread_delay(800);
+    }
+}
+
+ALIGN(RT_ALIGN_SIZE)
+static char task3_stack[512];
+static struct rt_thread task3_thread;
+static rt_sem_t gpioa_sem = RT_NULL;
+
+/*********************************************************************
+ * @fn      task3_entry
+ *
+ * @brief   task3任务函数
+ *
+ * @return  none
+ */
+void task3_entry(void *parameter)
+{
+    gpioa_sem = rt_sem_create("gpioa sem", 0, RT_IPC_FLAG_FIFO);
+    if(gpioa_sem != NULL)
+    {
+        GPIOA_ModeCfg(GPIO_Pin_12, GPIO_ModeIN_PU);
+        GPIOA_ITModeCfg(GPIO_Pin_12, GPIO_ITMode_FallEdge);
+        PFIC_EnableIRQ(GPIO_A_IRQn);
+        while(1)
+        {
+            rt_sem_take(gpioa_sem, RT_WAITING_FOREVER); /* 等待信号量 */
+            rt_kprintf("gpioa sem get\r\n");
+        }
+    }
+    else
+    {
+        rt_kprintf("sem create failed\r\n");
+    }
+}
+
+ALIGN(RT_ALIGN_SIZE)
+static char tmos_stack[1024];
+static struct rt_thread tmos_thread;
+
+__attribute__((aligned(4))) uint32_t MEM_BUF[BLE_MEMHEAP_SIZE / 4];
+#if (defined (BLE_MAC)) && (BLE_MAC == TRUE)
+const uint8_t MacAddr[6] = {0x84, 0xC2, 0xE4, 0x03, 0x32, 0x22};
+#endif
+
+/*********************************************************************
+ * @fn      tmos_task
+ *
+ * @brief   tmos program.
+ *
+ * @param  *pvParameters - Parameters point of task2
+ *
+ * @return  none
+ */
+__HIGH_CODE
+void tmos_task(void *pvParameters)
+{
+    rt_base_t level;
+
+    level = rt_hw_interrupt_disable();
+    CH59x_BLEInit();
+    HAL_Init();
+    GAPRole_PeripheralInit();
+    Peripheral_Init();
+    rt_kprintf("%08x,%08x\n", PFIC->VTFADDR[2], PFIC->VTFADDR[3]);
+    SetVTFIRQ((uint32_t)0, BLEB_IRQn, 2, DISABLE);
+    SetVTFIRQ((uint32_t)0, BLEL_IRQn, 3, DISABLE);
+    rt_kprintf("%08x,%08x\n", PFIC->VTFADDR[2], PFIC->VTFADDR[3]);
+    PFIC_SetPriority(BLEL_IRQn, 0xe0);   /* 必须保证不可抢断其他任务 */
+    rt_hw_interrupt_enable(level);
+    rt_kprintf("tmos ble start\n");
+    while (1)
+    {
+        TMOS_SystemProcess();
+    }
+}
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   主函数
+ *
+ * @note    main is one of threads in rt-thread.
+ *
+ * @return  none
+ */
+int main()
+{
+    rt_enter_critical();
+
+    rt_thread_init(&task1_thread,
+                   "task1",
+                   task1_entry,
+                   RT_NULL,
+                   &task1_stack[0],
+                   sizeof(task1_stack),
+                   4, 20);
+    rt_thread_startup(&task1_thread);
+
+    rt_thread_init(&task2_thread,
+                   "task2",
+                   task2_entry,
+                   RT_NULL,
+                   &task2_stack[0],
+                   sizeof(task2_stack),
+                   4, 20);
+    rt_thread_startup(&task2_thread);
+
+    rt_thread_init(&task3_thread,
+                   "task3",
+                   task3_entry,
+                   RT_NULL,
+                   &task3_stack[0],
+                   sizeof(task3_stack),
+                   4, 20);
+    rt_thread_startup(&task3_thread);
+
+    rt_thread_init(&tmos_thread,
+                   "tmos",
+                   tmos_task,
+                   RT_NULL,
+                   &tmos_stack[0],
+                   sizeof(tmos_stack),
+                   RT_THREAD_PRIORITY_MAX - 2, 20);
+    rt_thread_startup(&tmos_thread);
+
+    rt_exit_critical();
+
+    return 0;
+}
+
+void msh_test_print(void)
+{
+    rt_kprintf("this is a test for msh.\n");
+}
+MSH_CMD_EXPORT(msh_test_print, this is a msh test);
+
+/*********************************************************************
+ * @fn      GPIOA_IRQHandler
+ *
+ * @brief   GPIOA_IRQHandler.
+ *
+ * @param   none
+ *
+ * @return  none
+ */
+__HIGH_CODE
+void GPIOA_IRQHandler(void)
+{
+    /* 本函数可以作为在本工程rt-thread nano中的中断函数写法示例 */
+    uint16_t flag;
+
+    flag = GPIOA_ReadITFlagPort();
+    if((flag & GPIO_Pin_12) != 0)
+    {
+        rt_sem_release(gpioa_sem);  /* 释放信号量 */
+    }
+    GPIOA_ClearITFlagBit(flag); /* 清除中断标志 */
+}
+
